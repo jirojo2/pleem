b2209c88e8334e46d4a66e3eb08340f5b2576d85
José Ignacio Rojo Rivero <jirojo2@gmail.com>
2015-10-13 19:33:33 +0200

    auth routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,10 +11,29 @@
 |
 */
 
+// Landing
 Route::get('/', function () {
     return view('welcome');
 });
 
+// Authentication
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
+
+// Password reset link request
+Route::get('password/email', 'Auth\PasswordController@getEmail');
+Route::post('password/email', 'Auth\PasswordController@postEmail');
+
+// Password reset
+Route::get('password/reset/{token}', 'Auth\PasswordController@getReset');
+Route::post('password/reset', 'Auth\PasswordController@postReset');
+
+// API
 Route::group(array('prefix' => 'api/v1'), function() {
 
     Route::get('/csrf-token', function() {
