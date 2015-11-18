b5369ac43c1c164542e1160e3bfc175e76585182
José Ignacio Rojo Rivero <jirojo2@gmail.com>
2015-11-18 21:10:47 +0100

    config resource and auth

---
+++ laravel/app/Http/Controllers/ConfigController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Gate;
+use Illuminate\Http\Request;
+
+use App\Config;
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class ConfigController extends Controller
+{
+    /**
+     * Show config
+     *
+     * @return Response
+     */
+    public function index()
+    {
+        if (Gate::denies('view-config')) {
+            abort(403);
+        }
+
+        $config = Config::firstOrCreate([
+            'registration_enabled' => true
+        ]);
+
+        return response()->json($config);
+    }
+
+    /**
+     * Update config via POST
+     *
+     * @param  Request  $request
+     * @return Response
+     */
+    public function store(Request $request)
+    {
+        if (Gate::denies('set-config')) {
+            abort(403);
+        }
+
+        $config = Config::first();
+        $config->update($request->only('registration_enabled'));
+        return response()->json($config);
+    }
+}
