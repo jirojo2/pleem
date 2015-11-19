d19148d94a771c01ed632abb942dbaf7a4fa7051
Camelia Cristache <camelia.cristache@binovate.com>
2015-11-19 02:54:30 +0200

    Working on edit idea.

--- laravel/public/assets/js/controller/idea.js
+++ laravel/public/assets/js/controller/idea.js
@@ -4,28 +4,49 @@ angular.module('ecaApp')
         $scope.submitted = false;
         $scope.name = '';
         $scope.description = '';
+        $scope.showIdea = true;
 
         User.userPromise().then(function(response) {
             $scope.user = response.data;
             API.Group.get({ groupId: $scope.user.group.id }, function(response){
                 $scope.idea = response.idea;
+                if ($scope.idea) {
+                    $scope.showIdea = false;
+                }
             });
         });
+
+        $scope.editIdea = function() {
+            $scope.showIdea = true;
+        }
 
         $scope.submitIdea = function() {
             $scope.submitted = true;
 
             if ($scope.ideaForm.$valid){
-                var newIdea = new API.Idea({ name: $scope.name, description: $scope.description });
-                newIdea.$save()
-                    .then(function ok(response) {
-                        console.log('yay');
-                        API.Group.get({ groupId: $scope.user.group.id }, function(response){
-                            $scope.idea = response.idea;
+                if ($scope.idea){
+                    var newIdea = new API.Idea({ ideaId: $scope.idea.id },{ name: $scope.name, description: $scope.description });
+                    newIdea.$update()
+                        .then(function ok(response) {
+                            API.Group.get({ groupId: $scope.user.group.id }, function(response){
+                                $scope.idea = response.idea;
+                                $scope.showIdea = false;
+                            });
+                        }, function err(response) {
+                            //do error stuff here
                         });
-                    }, function err(response) {
-                        //do error stuff here
-                    });
+                } else {
+                    var newIdea = new API.Idea({ name: $scope.name, description: $scope.description });
+                    newIdea.$save()
+                        .then(function ok(response) {
+                            API.Group.get({ groupId: $scope.user.group.id }, function(response){
+                                $scope.idea = response.idea;
+                                $scope.showIdea = false;
+                            });
+                        }, function err(response) {
+                            //do error stuff here
+                        });
+                }
             }
         }
     }
