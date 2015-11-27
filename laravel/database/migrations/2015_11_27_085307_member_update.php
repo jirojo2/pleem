890c08bbd08903eae923a36b1e060b3aa3eb54ef
José Ignacio Rojo <jirojo2@gmail.com>
2015-11-27 11:06:22 +0100

    fix typo

--- laravel/database/migrations/2015_11_27_085307_member_update.php
+++ laravel/database/migrations/2015_11_27_085307_member_update.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class ConfigAdminEmail extends Migration
+class MemberUpdate extends Migration
 {
     /**
      * Run the migrations.
