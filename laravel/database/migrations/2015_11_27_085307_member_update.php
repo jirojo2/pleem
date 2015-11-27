4dc3a94807acc5c83f5ab31e044e4b0b07d608bc
José Ignacio Rojo <jirojo2@gmail.com>
2015-11-27 11:02:05 +0100

    more member fields

---
+++ laravel/database/migrations/2015_11_27_085307_member_update.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConfigAdminEmail extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('members', function ($table) {
+            $table->integer('years_study');
+            $table->string('study_level');
+            $table->string('faculty');
+            $table->string('department');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('members', function ($table) {
+            $table->dropColumn(['years_study', 'study_level', 'faculty', 'department']);
+        });
+    }
+}
