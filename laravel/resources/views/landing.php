2a241b4e3664d8e2e42c2a41467abf2bcd6ea44f
Bogdan Arvinte <bogdan.arvinte@eestec.ro>
2015-12-08 00:56:48 +0200

    Add image for Vilim Roller and FB share thumbnail

--- laravel/resources/views/landing.php
+++ laravel/resources/views/landing.php
@@ -6,6 +6,7 @@
         <meta charset="utf-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta property="og:image" content="assets/images/social-logo.png" />
 
         <!-- Site Properities -->
         <title>ECA 2015-2016</title>
